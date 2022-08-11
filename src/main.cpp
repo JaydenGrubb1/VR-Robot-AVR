c795c6b1c12aae0502b603eb56e6f5a61a823737
Jayden Grubb <jaydengrubb1@gmail.com>
2022-08-11 17:16:28 +1000

    added servo driver library and test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,51 @@
-#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+#define OSC_FREQ 25000000
+#define PWM_FREQ 50
+#define PWM_MIN 125
+#define PWM_MAX 570
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 void setup()
 {
-	// put your setup code here, to run once:
-	pinMode(LED_BUILTIN, OUTPUT);
-	Serial.begin(9600);
+	pwm.begin();
+	pwm.setOscillatorFrequency(OSC_FREQ);
+	pwm.setPWMFreq(PWM_FREQ);
+
+	for (size_t i = 0; i < 16; i++)
+	{
+		pwm.setPWM(i, 4096, 0);
+	}
+}
+
+// Servo 0 - no response but significant current draw - dead?
+// Servo 1 - good
+// Servo 2 - no response
+// Servo 3 - sometimes no response - bad wiring?
+// Servo 4 - good
+// Servo 5 - good
+void setServo(int servo, int angle)
+{
+	int pulse = map(angle, 0, 180, PWM_MIN, PWM_MAX);
+	pwm.setPWM(servo, 0, pulse);
 }
 
 void loop()
 {
-	// put your main code here, to run repeatedly:
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(200);
-	digitalWrite(LED_BUILTIN, LOW);
-	delay(200);
+	int servo = 7;
+
+	for (size_t i = 1; i < 180; i++)
+	{
+		setServo(servo, i);
+		delay(5);
+	}
+	delay(100);
+	for (size_t i = 180; i > 0; i--)
+	{
+		setServo(servo, i);
+		delay(5);
+	}
+	delay(100);
 }
